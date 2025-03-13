633f9b9d820b8db4784afda643b75ad1eaa67a29
TeaShubham <thangeshubham7276@gmail.com>
2025-03-13 17:43:23 +0530

    new files added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,16 @@
+import "./Home.css";
+
 const Home = () => {
-    return <h2>Welcome to My Portfolio</h2>;
-  };
-  
-  export default Home;
-  
+  return (
+    <div className="home-container">
+      <h1 className="home-title">Welcome to My Portfolio</h1>
+      <p className="home-subtitle">I am a Web Developer & Designer</p>
+      <div className="home-buttons">
+        <a href="#projects">View Projects</a>
+        <a href="#contact">Contact Me</a>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
